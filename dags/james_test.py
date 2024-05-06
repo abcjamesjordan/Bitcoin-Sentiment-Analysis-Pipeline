3b708370e490ed3c9b50fe5a07a6b346d87843bb
abcjamesjordan <a.github@abcjamesjordan.com>
2024-05-06 10:24:12 -0600

    Testing date options

--- dags/james_test.py
+++ dags/james_test.py
@@ -1,22 +1,26 @@
-from airflow import DAG
-from airflow.decorators import task, dag
 from datetime import datetime, timedelta
+from airflow.decorators import task, dag
 
 default_args = {
     'start_date': datetime(2024, 5, 3),
-    'email': ['your_email@example.com'],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    # 'email': ['your_email@example.com'],
+    # 'email_on_failure': True,
+    # 'email_on_retry': False,
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
 }
 
-@dag(default_args=default_args, schedule_interval='* * * * *')
-def james_test():
-    @task
-    def my_test_function():
-        print(datetime.today().strftime('%A'))
+@task
+def my_test_function(execution_date: datetime, logical_date: datetime):
+    TODAY = logical_date
+    print(f"TODAY: {TODAY}")
 
+
+@dag(
+        default_args=default_args
+        # schedule_interval='* * * * *'
+    )
+def james_test():
     run_this = my_test_function()
 
-james_test = james_test()
+james_test()
